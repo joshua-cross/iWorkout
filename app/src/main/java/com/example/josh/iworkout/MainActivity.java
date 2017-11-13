ada1fb4cb2d2def602868e0cad8e341f87d86f42
Josh Cross <crossjoshua6@gmail.com>
2017-11-13 16:10:18 +0000

    Initial page (edit days going to gym) has now been given a proper layout and a new app bar has been created.

--- app/src/main/java/com/example/josh/iworkout/MainActivity.java
+++ app/src/main/java/com/example/josh/iworkout/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.josh.iworkout;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(toolbar);
+        ActionBar app_bar = getSupportActionBar();
+        app_bar.setDisplayShowTitleEnabled(false);
+
+    }
+
+
+    //for the drop down menu.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String menu1 = getResources().getString(R.string.menu1);
+        //String menu2 = getResources().getString(R.string.menuText2);
+        //String menu3 = getResources().getString(R.string.menuText3);
+        //String menu4 = getResources().getString(R.string.menuText4);
+        //String menu5 = getResources().getString(R.string.menuText5);
+
+        if(item.toString().equals(menu1)) {
+            Intent days = new Intent (this, MainActivity.class);
+            startActivity(days);
+        } /*else if(item.toString().equals(menu2)) {
+            Intent editPage = new Intent (this, ChartMonthlyPreview.class);
+            startActivity(editPage);
+        } else if(item.toString().equals(menu3)) {
+            Intent editPage = new Intent (this, ChartYearlyPreview.class);
+            startActivity(editPage);
+        } else if(item.toString().equals(menu4)) {
+            Intent editPage = new Intent (this, Edit.class);
+            startActivity(editPage);
+        } else if(item.toString().equals(menu5)) {
+            Intent editPage = new Intent (this, EditGoalsAndSleepTimes.class);
+            startActivity(editPage);
+        }
+        */
+        return super.onOptionsItemSelected(item);
     }
 }
